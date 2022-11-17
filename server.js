22fa729e402f1090bd3104254c0e285b3fc58382
boubacarsow <42849861+boubacarsow@users.noreply.github.com>
2022-11-17 10:19:00 -0700

    updating app

---
+++ server.js
@@ -0,0 +1,67 @@
+//require
+const express = require('express');
+const app = express();
+const fruits = require('./models/fruits');
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+// connection to databse 
+
+
+
+//middleware
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+app.use(express.urlencoded({extended:false}));
+
+//routes
+//I.N.D.U.C.E.S 
+
+//index
+app.get('/fruits', (req,res) => {
+    res.render('Index', {
+        fruits: fruits
+    });
+});
+
+
+//New 
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+
+//delete 
+
+
+//updated 
+
+
+//create 
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.send('data received');
+});
+
+//Edit 
+
+
+//show
+
+app.get("/fruits/:index", (req, res) => {
+    res.render("Show", {
+      fruit: fruits[req.params.index],
+    });
+  });
+
+app.listen(3000, () => {
+    console.log('listening');
+});
